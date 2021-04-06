449ad54fbb94b7cc9b2dd4a691796515b0d42bc8
Finley Knapman <f.g.knapman@student.reading.ac.uk>
2021-04-07 00:28:52 +0100

    Infractions text touch-up

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -33,9 +33,9 @@ exports.execute = (interaction) => {
                     .setAuthor(`UserInfo for ${targetMember.user.tag}`)
                     .addField("General User Data", `**User ID**: ${targetMember.id}\n**Account Mention**: ${targetMember}\n**Is a Bot?**: ${targetMember.user.bot}\n**Created At**: ${df(targetMember.user.createdTimestamp, "dd/mm/yyyy HH:MM:ss Z")}`)
                     .addField("Global Valkyrie Data", `**Messages Sent**: ${userData.globalMsgCount}\n**Last Seen** in ${lastSeenGuildName} (${lastSeenChannelName}) at ${df(userData.lastSeenTime, "dd/mm/yyyy HH:MM:ss Z")}`)
-                    .addField("Global Infractions", `**Warns**:${userData.globalWarnCount}\n**Mutes**:${userData.globalMuteCount}\n**Kicks**:${userData.globalKickCount}\n**Bans**:${userData.globalBanCount}`, true)
+                    .addField("Global Infractions", `**Warns**: ${userData.globalWarnCount}\n**Mutes**: ${userData.globalMuteCount}\n**Kicks**: ${userData.globalKickCount}\n**Bans**:  ${userData.globalBanCount}`, true)
                     .addField("This Guild Data", `**Messages Sent**: ${guildUserData.guildMsgCount}\n**Joined At**: ${df(guildUserData.createdAt, "dd/mm/yyyy HH:MM:ss Z")}`)
-                    .addField("This Guild Infractions", `**Warns**:${guildUserData.guildWarnCount}\n**Mutes**:${guildUserData.guildMuteCount}\n**Kicks**:${guildUserData.guildKickCount}\n**Bans**:${guildUserData.guildBanCount}`, true)
+                    .addField("This Guild Infractions", `**Warns**: ${guildUserData.guildWarnCount}\n**Mutes**: ${guildUserData.guildMuteCount}\n**Kicks**: ${guildUserData.guildKickCount}\n**Bans**: ${guildUserData.guildBanCount}`, true)
                     .setColor("#00C597")
                     .setFooter("userinfo.interactions.valkyrie")
                     .setTimestamp(new Date());
